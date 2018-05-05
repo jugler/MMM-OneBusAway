a005ec425b519f5dcfefb76b5ad0d531b45e5898
Jesus Silva <jugler@gmail.com>
2018-05-04 22:48:24 -0700

    Format code

--- MMM-OneBusAway.js
+++ MMM-OneBusAway.js
@@ -1,27 +1,27 @@
 "use strict";
 
-Module.register("MMM-OneBusAway",{
+Module.register("MMM-OneBusAway", {
     result: [],
-	// Default module config.
-	defaults: {
+    // Default module config.
+    defaults: {
         stopId: "1_905",
-        fadeSpeed: 1000*60 // update every minute
-	},
+        fadeSpeed: 1000 * 60 // update every minute
+    },
 
-	// Override dom generator.
-	getDom: function() {
+    // Override dom generator.
+    getDom: function () {
         var wrapper = document.createElement("div");
         wrapper.className = "oneBusAway";
-        if (this.hasLoaded == false){   //No data has arrived
+        if (this.hasLoaded == false) {   //No data has arrived
             var loadingMessage = document.createElement("span");
             loadingMessage.innerHTML = "Checking Bus status...";
-            wrapper.appendChild(loadingMessage); 
-        }else if (this.result.length==0){ //No buses right now
+            wrapper.appendChild(loadingMessage);
+        } else if (this.result.length == 0) { //No buses right now
             var noBuses = document.createElement("span");
             noBuses.innerHTML = "Checking Bus status...";
-            wrapper.appendChild(noBuses); 
+            wrapper.appendChild(noBuses);
             this.message = "No buses departing soon";
-        } else{ //extract times of arrival for the buses
+        } else { //extract times of arrival for the buses
             for (var departureIndex = 0; departureIndex < this.result.length && departureIndex < 5; departureIndex++) {
                 var departureDetails = this.result[departureIndex]
                 var busShortName = departureDetails['routeShortName'];
@@ -29,11 +29,11 @@ Module.register("MMM-OneBusAway",{
                 var busEntry = this.getBusEntry(busShortName, busDepartureTimeStamp);
                 wrapper.appendChild(busEntry);
             }
-        }        
-		return wrapper;
+        }
+        return wrapper;
     },
 
-    getBusEntry: function(route, arrival){
+    getBusEntry: function (route, arrival) {
         var busIcon = document.createElement("img");
         busIcon.className = "badge";
         busIcon.src = "modules/MMM-OneBusAway/oba_logo.png";
@@ -43,8 +43,8 @@ Module.register("MMM-OneBusAway",{
         var nextStopText = document.createElement("span");
         var busDepartureDate = new Date(arrival);
         var dateNow = new Date();
-        var departingInMinutes = (busDepartureDate - dateNow)/60000; //difference in minutes
-        if (departingInMinutes>0){
+        var departingInMinutes = (busDepartureDate - dateNow) / 60000; //difference in minutes
+        if (departingInMinutes > 0) {
             departingInMinutes = departingInMinutes.toFixed(0);
             var departureMessage = "  <b>" + route + "</b> in " + departingInMinutes + " mins";
             nextStopText.innerHTML = departureMessage;
@@ -54,26 +54,26 @@ Module.register("MMM-OneBusAway",{
         return nextStop;
     },
 
-    getStyles: function() {
-		return ["MMM-OneBusAway.css"];
-	},
-    
-    start: function() {
+    getStyles: function () {
+        return ["MMM-OneBusAway.css"];
+    },
+
+    start: function () {
         this.hasLoaded = false;
 
         this.getBusesInfo();
         var self = this;
-        setInterval(function() {
+        setInterval(function () {
             self.getBusesInfo(); // no speed defined, so it updates instantly.
         }, self.config.fadeSpeed); //perform every 1000 milliseconds.
-        
+
     },
-    
+
     getBusesInfo: function () {
         this.sendSocketNotification('GET_BUSES_INFO', this.config.stopId);
     },
 
-    socketNotificationReceived: function(notification, payload) {
+    socketNotificationReceived: function (notification, payload) {
         if (notification === "BUSES_INFO") {
             this.hasLoaded = true;
             this.result = payload;
--- node_helper.js
+++ node_helper.js
@@ -10,22 +10,22 @@ module.exports = NodeHelper.create({
     var self = this;
     var url = "http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/" + stopId + ".json?key=TEST";
     request({ url: url, method: 'GET' }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var result = JSON.parse(body);
-            var arrivalsAndDepartures = result['data']['entry']['arrivalsAndDepartures']
-            console.log ("Got arrivals and departures data, size count:" + arrivalsAndDepartures.length);
-            self.sendSocketNotification('BUSES_INFO', arrivalsAndDepartures); 
-        }
-        else {
-            console.log("ERROR while getting the url" + error);
-        }
+      if (!error && response.statusCode == 200) {
+        var result = JSON.parse(body);
+        var arrivalsAndDepartures = result['data']['entry']['arrivalsAndDepartures']
+        console.log("Got arrivals and departures data, size count:" + arrivalsAndDepartures.length);
+        self.sendSocketNotification('BUSES_INFO', arrivalsAndDepartures);
+      }
+      else {
+        console.log("ERROR while getting the url" + error);
+      }
     });
   },
 
   //Subclass socketNotificationReceived received.
-  socketNotificationReceived: function(notification, payload) {
+  socketNotificationReceived: function (notification, payload) {
     if (notification === 'GET_BUSES_INFO') {
-        this.getBusesInfo(payload);
+      this.getBusesInfo(payload);
     }
   }
 
